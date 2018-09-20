f5b70272f343c7e904a52eb3c80fb60d2fee29a4
JuliermeH <julierme.jadon@outlook.com>
2018-09-20 05:42:13 -0300

    Adicionando JSF
    Adicionando Service e página JSF

---
+++ cartaocredito/src/main/java/br/edu/ifpb/dac/cartaocredito/Cadastro.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifpb.dac.cartaocredito;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author IFPB
+ */
+public interface Cadastro {
+    
+    public void cadastrar(Cartao cartao);
+    
+}
---
+++ cartaocredito/src/main/java/br/edu/ifpb/dac/cartaocredito/CadastroService.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifpb.dac.cartaocredito;
+
+import java.time.LocalDate;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+/**
+ *
+ * @author IFPB
+ */
+@ManagedBean
+@SessionScoped
+public class CadastroService implements Cadastro{
+    
+    private Cartao cartao = new Cartao();
+    
+    @Override
+    public void cadastrar(Cartao cartao) {
+        this.cartao;
+    }
+    
+}
