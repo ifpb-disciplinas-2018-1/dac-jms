2af209aa1270e90259381c8218f6209a58eb5b57
FlavioHenriique <flaviohenrique638@gmail.com>
2018-09-20 08:51:47 -0300

    Criando interface Email e classe que irá implementá-la

---
+++ EmailPedido/src/main/java/br/edu/ifpb/emailpedido/EnviarEmail.java
@@ -0,0 +1,12 @@
+
+package br.edu.ifpb.emailpedido;
+
+public class EnviarEmail implements Email{
+
+    @Override
+    public void enviar(String destinatario, String titulo, String corpo) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+}
