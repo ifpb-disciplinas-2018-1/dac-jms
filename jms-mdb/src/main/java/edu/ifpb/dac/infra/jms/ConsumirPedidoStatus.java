7831fa929e5dca2f031b7890e8b62b5cfd2d9e55
Lucas Duete <lm.duete@gmail.com>
2018-09-22 21:54:12 -0300

    Implementando Consumidor de Status de Pedido

---
+++ jms-mdb/src/main/java/edu/ifpb/dac/infra/jms/ConsumirPedidoStatus.java
@@ -0,0 +1,46 @@
+package edu.ifpb.dac.infra.jms;
+
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.inject.Inject;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.json.Json;
+import javax.json.JsonObject;
+import java.io.StringReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@MessageDriven(
+        activationConfig = {
+                @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Topic"),
+                @ActivationConfigProperty(propertyName = "destination", propertyValue = "topic"),
+                @ActivationConfigProperty(propertyName = "destinationLookup", propertyValue = "java:global/jms/aprovado"),
+                @ActivationConfigProperty(propertyName = "messageSelector", propertyValue = "typeMessage!='email'")
+        }
+)
+public class ConsumirPedidoStatus implements MessageListener {
+
+    @Inject
+    private EnviarStatusPedido enviarStatusPedido;
+
+    @Override
+    public void onMessage(Message message) {
+        try {
+            String body = message.getBody(String.class);
+
+
+            JsonObject jsonPedido = Json
+                    .createReader(
+                            new StringReader(body)
+                    )
+                    .readObject();
+
+            enviarStatusPedido.sender(jsonPedido);
+
+        } catch (Exception ex) {
+            Logger.getLogger(ConsumirPedidoStatus.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+}
